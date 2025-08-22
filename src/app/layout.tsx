94bfca3a2b5a8dbeb631a97726cd41b1c91742c9
Carlos <c.diazadriann@gmail.com>
2025-08-22 15:27:49 -0400

    Mejoras

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,29 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Montserrat } from "next/font/google"
-import { Open_Sans } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import { Open_Sans } from "next/font/google";
+import "./globals.css";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-montserrat",
   weight: ["400", "600", "700", "900"],
-})
+});
 
 const openSans = Open_Sans({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-open-sans",
   weight: ["400", "500", "600"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Lotto Latina Resultados Loterias",
   description:
-     "Descarga Lotto Latina y disfruta de la mejor experiencia de lotería en tu móvil. Resultados de todos los sorteos y loterias de toda America Latina, Centro America, en tiempo real, fácil de usar y totalmente seguro.",
-  keywords: [
-    "LottoLatina",
-    "Lotto latina",
-    "Lotto Latina Resultados Loterias",
-
-  ],
-  authors: [
-    { name: "Carlos Jose Diaz", url: "https://www.lottolatina.com" },
-  ],
+    "Descarga Lotto Latina y disfruta de la mejor experiencia de lotería en tu móvil. Resultados de todos los sorteos y loterias de toda America Latina, Centro America, en tiempo real, fácil de usar y totalmente seguro.",
+  keywords: ["LottoLatina", "Lotto latina", "Lotto Latina Resultados Loterias"],
+  authors: [{ name: "Carlos Jose Diaz", url: "https://www.lottolatina.com" }],
   creator: "DiazCode",
   publisher: "DiazCode",
   robots: {
@@ -72,11 +65,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" className={`${montserrat.variable} ${openSans.variable} antialiased`}>
+    <html
+      lang="es"
+      className={`${montserrat.variable} ${openSans.variable} antialiased`}
+    >
       <body className="font-sans">{children}</body>
     </html>
-  )
+  );
 }
