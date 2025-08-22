63dd64fbebf5d510345f24ec343e4b3e6e08700c
Carlos <c.diazadriann@gmail.com>
2025-08-22 15:24:53 -0400

    Mejoras

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,53 @@ const openSans = Open_Sans({
 export const metadata: Metadata = {
   title: "Lotto Latina Resultados Loterias",
   description:
-    "Descarga Lotto Latina y disfruta de la mejor experiencia de lotería en tu móvil. Resultados de todos los sorteos y loterias de toda America Latina, Centro America, en tiempo real, fácil de usar y totalmente seguro.",
-}
+     "Descarga Lotto Latina y disfruta de la mejor experiencia de lotería en tu móvil. Resultados de todos los sorteos y loterias de toda America Latina, Centro America, en tiempo real, fácil de usar y totalmente seguro.",
+  keywords: [
+    "LottoLatina",
+    "Lotto latina",
+    "Lotto Latina Resultados Loterias",
+
+  ],
+  authors: [
+    { name: "Carlos Jose Diaz", url: "https://www.lottolatina.com" },
+  ],
+  creator: "DiazCode",
+  publisher: "DiazCode",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  openGraph: {
+    title: "LottoLatina - Resultados de Loterias",
+    description:
+      "Descarga Lotto Latina y disfruta de la mejor experiencia de lotería en tu móvil.",
+    url: "https://www.lottolatina.com",
+    siteName: "LottoLatina",
+    images: [
+      {
+        url: "https://www.lottolatina.com/logo.png",
+        width: 500,
+        height: 500,
+        alt: "Logo LottoLatina",
+      },
+    ],
+    locale: "es_DO",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://www.lottolatina.com",
+    languages: {
+      "es-DO": "https://www.lottolatina.com",
+      "en-US": "https://www.lottolatina.com",
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
